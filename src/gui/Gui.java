55d0fcefc91d2b929d98bd47043f0b7e092e7a51
markk <markk@LAPTOP-KLMCBK4R>
2017-03-12 18:32:01 -0400

    added fractal panel

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -2,10 +2,8 @@ package gui;
 
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
-
 import edu.buffalo.cse116.Model;
-
-
+import edu.buffalo.fractal.FractalPanel;
 
 
 public class Gui extends JFrame {
@@ -14,11 +12,13 @@ public class Gui extends JFrame {
 	
 	public  Gui(Model model) {
 		super("Fractal Builder");
+		FractalPanel f = new FractalPanel();
 		_model = model;
 		this.setSize(600, 600);
 	    this.add(new DropDownMeun(_model), BorderLayout.NORTH);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(f);
 	}
 
 	
